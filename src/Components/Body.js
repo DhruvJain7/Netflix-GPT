9e374c44b176bf8724f4a544387a01681383cc54
Dhruv Jain <djmjtj10@gmail.com>
2025-09-01 22:45:34 +0530

    Redux Store Setup & Navigation to Browse Page

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,8 +2,30 @@ import React from 'react';
 import Browse from './Browse';
 import Login from './Login';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { auth } from '../utils/firebase';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Body = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+
+        // ...
+      } else {
+        dispatch(removeUser());
+        // User is signed out
+        // ...
+      }
+    });
+  }, []);
   const appRouter = createBrowserRouter([
     {
       path: '/',
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Header from './Header';
 import { useState, useRef } from 'react';
 import { checkValidData } from '../utils/validate';
-import { use } from 'react';
+
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-
+  const navigate = useNavigate();
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
@@ -36,6 +37,7 @@ const Login = () => {
           // Signed up
           const user = userCredential.user;
           console.log(user);
+          navigate('./Browse');
           // ...
         })
         .catch((error) => {
@@ -54,6 +56,7 @@ const Login = () => {
           // Signed in
           const user = userCredential.user;
           console.log(user);
+          navigate('./Browse');
           // ...
         })
         .catch((error) => {
